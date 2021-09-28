2e4006082f7bd8d526a52563d4352228a4870068
tekthree <tekthree.gmail.com>
2021-09-28 15:41:50 -0700

    testing jquery

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import './style.css'
 
 
-
 $('.hamburger').on('click', function() {
   $('.hamburger').toggleClass('is-active');
   $('.popout-menu').toggleClass('menu-open');
   $('.popout-nav').toggleClass('popout-inactive');
 });
+
 
