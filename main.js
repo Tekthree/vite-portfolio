cc838d78d341763640d6d0e39808c8f22a6c049e
tekthree <tekthree.gmail.com>
2021-09-29 14:48:56 -0700

    adds API to backend for text content, fixes hamburger zindex

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
 import './style.css'
 
+// -------------- hamburger menu button ------------------- //
 
 $('.hamburger').on('click', function() {
   $('.hamburger').toggleClass('is-active');
   $('.popout-menu').toggleClass('menu-open');
   $('.popout-nav').toggleClass('popout-inactive');
+  $('body').toggleClass('overhidden');
 });
 
+
+
+// -------------- API call to backend ------------------- //
+async function getBlock(){
+  await axios.get('http://localhost:3001/block-children')
+    .then(res =>{
+      console.log('success', res.data.results[3].paragraph.text[0].plain_text)
+
+      let herotext = res.data.results[3].paragraph.text[0].plain_text
+
+      $('.mw').text(herotext);
+
+    })
+    .catch(err => console.error(error))
+}
+
+getBlock()
 
